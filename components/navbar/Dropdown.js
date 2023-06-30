f3392b996e0384abb621a304da1585e6c690a5a1
Nicholas-Stokes <plexagongb@gmail.com>
2023-06-30 01:11:15 -0400

    Update Dropdown.js

--- components/navbar/Dropdown.js
+++ components/navbar/Dropdown.js
@@ -6,7 +6,7 @@ export function Dropdown ({searchText}) {
     const filteredTeams = teams.filter((team)=>team.toLowerCase().includes(searchText.toLowerCase()))
     const teamsList = filteredTeams.map((team,index) => (
         // <span className="flex  p-1 rounded-md w-48 text-zinc-200 text-center text-sm tracking-wider font-thin" key={index}>{team}</span>)
-        <TeamLink teamInfo={team}/>));
+        <TeamLink key={index} teamInfo={team}/>));
     return (<div>
         <div className="grid mt-10 grid-rows-10 grid-cols-1 overflow-x-clip w-48 max-h-40 bg-zinc-700 rounded-md">
              {teamsList}
