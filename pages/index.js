0818f26c83780d9210464d8456500ee362dffc66
Nicholas-Stokes <plexagongb@gmail.com>
2025-04-18 13:10:42 -0400

    Added event key option to alliance match score predictor

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,41 @@
 
-import React from "react";
+import {React, useState} from "react";
 import useSWR from "swr";
 import Head from "next/head.js";
 import axios from "axios";
 import {Navbar} from "@/components/navbar"
 import { useFormStatus } from "react-dom";
 
+
 const fetcher = url => axios.get(url).then(res => res.data)
 
 
-function Submit() {
+
+function Submit({scoreLoad}) {
   //const { pending } = useFormStatus();
   return (
     <button className='appearance-none rounded-md mr-20 text-sm font-thin tracking-wide text-zinc-200 p-1.5 ring-2 caret-cyan-400 ring-cyan-400 outline-none bg-zinc-900 peer' type="submit" disabled={false}>
-      {false ? "Calculating..." : "Calculate Alliance Score"}
+      {scoreLoad ? "Calculate Alliance Score" : "Calculating..."}
     </button>
   );
 }
 
 export default function Index() {
+
+  const [scoreLoad, setScoreLoad] = useState(true);
   async function getTeamPrediction(event) {
     event.preventDefault();
     var formData = new FormData(document.querySelector("form"));
-    var res = await fetch("https://statbotics-but-bad-production.up.railway.app/sim/year/2025/teams/frc"+formData.get("team1").toString()+"/frc"+formData.get("team2").toString()+"/frc"+formData.get("team3").toString())
+    console.log(formData);
+    setScoreLoad(false);
+    if (formData.get("event") == "") {
+      var res = await fetch("https://statbotics-but-bad-production.up.railway.app/sim/year/2025/teams/frc"+formData.get("team1").toString()+"/frc"+formData.get("team2").toString()+"/frc"+formData.get("team3").toString())
+    }
+    else {
+      var res = await fetch("https://statbotics-but-bad-production.up.railway.app/sim/year/2025/teams/frc"+formData.get("team1").toString()+"/frc"+formData.get("team2").toString()+"/frc"+formData.get("team3").toString()+"/event/"+formData.get("event"));
+    }
     const response = await res.json()
+    setScoreLoad(true)
     var tBody = document.getElementById("tableBody");
     var tList = document.createElement("tr");
     for (var data in response) {
@@ -47,12 +59,14 @@ export default function Index() {
     
     <form onSubmit={getTeamPrediction}>
       <label htmlFor="team1">Enter team number: </label>
-      <input className='appearance-none rounded-md mr-20 text-sm font-thin tracking-wide text-zinc-200 p-1.5 ring-2 caret-cyan-400 ring-cyan-400 outline-none bg-zinc-900 peer' type ="number" name="team1" id="team1"></input>
+      <input className='appearance-none rounded-md mr-20 text-sm font-thin tracking-wide text-zinc-200 p-1.5 ring-2 caret-cyan-400 ring-cyan-400 outline-none bg-zinc-900 peer' type ="number" name="team1" id="team1" required></input>
       <label htmlFor="team2">Enter team number: </label>
-      <input className='appearance-none rounded-md mr-20 text-sm font-thin tracking-wide text-zinc-200 p-1.5 ring-2 caret-cyan-400 ring-cyan-400 outline-none bg-zinc-900 peer' type ="number" name="team2" id="team2"></input>
+      <input className='appearance-none rounded-md mr-20 text-sm font-thin tracking-wide text-zinc-200 p-1.5 ring-2 caret-cyan-400 ring-cyan-400 outline-none bg-zinc-900 peer' type ="number" name="team2" id="team2" required></input>
       <label htmlFor="team3">Enter team number: </label>
-      <input className='appearance-none rounded-md mr-20 text-sm font-thin tracking-wide text-zinc-200 p-1.5 ring-2 caret-cyan-400 ring-cyan-400 outline-none bg-zinc-900 peer' type ="number" name ="team3" id="team3"></input>
-     <Submit />
+      <input className='appearance-none rounded-md mr-20 text-sm font-thin tracking-wide text-zinc-200 p-1.5 ring-2 caret-cyan-400 ring-cyan-400 outline-none bg-zinc-900 peer' type ="number" name ="team3" id="team3" required></input>
+      <label htmlFor="event">Enter event key (Optional): </label>
+      <input className='appearance-none rounded-md mr-20 text-sm font-thin tracking-wide text-zinc-200 p-1.5 ring-2 caret-cyan-400 ring-cyan-400 outline-none bg-zinc-900 peer' type ="text" name ="event" id="event"></input>
+     <Submit scoreLoad={scoreLoad}/>
     </form>
     <table className='border-separate border-spacing-2 border border-cyan-400 appearance-none rounded-md mt-10 mr-20 text-sm font-thin tracking-wide text-zinc-200 p-2 outline-none bg-zinc-900 '>
       <thead>
